b6e263c37693e4325edfd545a252f0a9a5142727
t2k269 <t2k269@gmail.com>
2014-06-03 23:45:48 -0700

    Add setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export TOP=`pwd` 
+export PATH=$TOP/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:~/bin:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
+export ARCH=arm
+export SUBARCH=arm
+
