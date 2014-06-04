0b9ad8efe6e31ee6877a452cb424fde66eca5533
t2k269 <t2k269@gmail.com>
2014-06-03 23:24:20 -0700

    New kitchen for 4.4.3 (Stock GPE)

--- kitchen/aik/build.sh
+++ kitchen/aik/build.sh
@@ -4,10 +4,10 @@ cd $TOP/kitchen/aik
 
 ./cleanup.sh
 
-./unpackimg.sh elementalx-boot.img
+./unpackimg.sh boot.img
 
-bin/dtbToolCM -o split_img/elementalx-boot.img-dtb -s 2048 -d "htc,project-id = <" -p $TOP/source/scripts/dtc/ $TOP/source/arch/arm/boot/
+bin/dtbToolCM -o split_img/boot.img-dtb -s 2048 -d "htc,project-id = <" -p $TOP/source/scripts/dtc/ $TOP/source/arch/arm/boot/
 
-cp -f $TOP/source/arch/arm/boot/zImage split_img/elementalx-boot.img-zImage 
+cp -f $TOP/source/arch/arm/boot/zImage split_img/boot.img-zImage 
 
 ./repackimg.sh
--- kitchen/build.sh
+++ kitchen/build.sh
@@ -4,5 +4,5 @@ $TOP/kitchen/aik/build.sh
 cp $TOP/kitchen/aik/image-new.img $TOP/kitchen/package/boot/boot.img
 
 cd $TOP/kitchen/package
-rm -f ../elementalx.zip
-zip -r -9 ../elementalx.zip * 
+rm -f ../flashable.zip
+zip -r -9 ../flashable.zip * 
