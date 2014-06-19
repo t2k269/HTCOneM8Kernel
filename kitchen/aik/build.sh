8e192fd51cfe98550f6730c12750563175d8ffbb
t2k269 <t2k269@gmail.com>
2014-06-19 01:52:36 -0700

    Update build scripts to add init.d support

--- kitchen/aik/build.sh
+++ kitchen/aik/build.sh
@@ -6,6 +6,17 @@ cd $TOP/kitchen/aik
 
 ./unpackimg.sh boot.img
 
+cd ramdisk
+grep sysinit init.rc > /dev/null
+if [ $? = 1 ]; then 
+	echo "Add support for init.d"
+	echo "" >> init.rc
+	echo "service sysinit /system/bin/sysinit" >> init.rc
+	echo "    oneshot" >> init.rc 
+fi 
+
+cd $TOP/kitchen/aik
+
 bin/dtbToolCM -o split_img/boot.img-dtb -s 2048 -d "htc,project-id = <" -p $TOP/source/scripts/dtc/ $TOP/source/arch/arm/boot/
 
 cp -f $TOP/source/arch/arm/boot/zImage split_img/boot.img-zImage 
--- kitchen/build.sh
+++ kitchen/build.sh
@@ -3,6 +3,14 @@
 $TOP/kitchen/aik/build.sh
 cp $TOP/kitchen/aik/image-new.img $TOP/kitchen/package/boot/boot.img
 
+cd $TOP/kitchen/package/system/lib/modules
+rm -f *.ko
+
 cd $TOP/kitchen/package
+find $TOP/source -name "*.ko" -exec cp -f {} $TOP/kitchen/package/system/lib/modules/ \;
+
+echo "Building flashable zip..."
 rm -f ../*.zip
-zip -r -9 ../kernel-m8-gpe-4.4.3-s2w-pattern.zip * 
+zip -r -9 -q ../kernel-m8-gpe-4.4.3-s2w-pattern.zip * 
+
+echo "Done."
