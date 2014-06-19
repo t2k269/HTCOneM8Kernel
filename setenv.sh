cd91c0fef02d3287cea6a7f96ad052faa34b570b
t2k269 <t2k269@gmail.com>
2014-06-18 22:57:04 -0700

    Update linaro toolchain to 2014.06

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export TOP=`pwd` 
-export PATH=$TOP/linaro/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.05/bin:~/bin:$PATH
+export PATH=$TOP/linaro/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.06/bin:~/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
