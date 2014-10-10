5d42b377b59fae9bb8516ad44eb351fab0ef43b4
t2k269 <t2k269@gmail.com>
2014-10-10 16:18:22 +0800

    Update linaro to 4.9.2-2014.09 and update the setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 export TOP=`pwd` 
-export PATH=$TOP/linaro/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.06/bin:~/bin:$PATH
+if [ "$1" = "linaro" ]; then
+	export PATH=$TOP/linaro/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.09/bin:~/bin:$PATH
+else
+	export PATH=$TOP/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:~/bin:$PATH
+fi
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 
+export PS1="HTCOneM8Kernel($TOP):\w$"
