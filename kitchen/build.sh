153695655ebbdf715f3f71412676e335e341bfe1
t2k269 <t2k269@gmail.com>
2014-10-12 00:34:01 +0800

    Add boot.img from cm build for the ramdisk and remove all other stuffs

--- kitchen/build.sh
+++ kitchen/build.sh
@@ -10,7 +10,7 @@ find $TOP/source -name "*.ko" -exec cp -f {} $TOP/kitchen/package/system/lib/mod
 echo "Building flashable zip..."
 rm -f $TOP/kitchen/*.zip
 
-SUFFIX=`grep CONFIG_LOCALVERSION= $TOP/source/arch/arm/configs/elementalx_defconfig | awk -F '"' '{print $2}'`
+SUFFIX=`grep CONFIG_LOCALVERSION= $TOP/source/arch/arm/configs/cm_m8_defconfig | awk -F '"' '{print $2}'`
 
 cd $TOP/kitchen/package
 zip -r -9 -q $TOP/kitchen/kernel${SUFFIX}.zip * 
