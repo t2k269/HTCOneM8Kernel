b51b4ad4ac858432fd6bcfa7b228d2c710db5314
t2k269 <t2k269@gmail.com>
2014-11-05 00:18:47 +0800

    Support dismiss keyguard in init.rd

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+function kbuild() {
+	pushd $TOP/kitchen
+	./build.sh
+	popd
+	ls $TOP/kitchen/*.zip
+}
+
+function kmake() {
+	pushd $TOP/source
+	make -j1
+	popd
+}
+
+function kflash() {
+	fastboot flash boot $TOP/kitchen/package/boot.img
+}
+
 export TOP=`pwd` 
 if [ "$1" = "linaro" ]; then
 	export PATH=$TOP/linaro/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.09/bin:~/bin:$PATH
@@ -13,3 +30,4 @@ export ARCH=arm
 export SUBARCH=arm
 
 export PS1="HTCOneM8Kernel($TOP):\w$"
+
