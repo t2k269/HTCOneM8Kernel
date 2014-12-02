e4d42279413281389a055d01c49b437c1bad9f0d
t2k269 <t2k269@gmail.com>
2014-12-02 10:21:48 +0800

    Add auto build script to build if new commit was detected, then upload to dropbox

---
+++ autobuild.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd $TOP/source
+
+git fetch cm11_htc_msm8974
+
+LAST_HASH=`git rev-parse remotes/cm11_htc_msm8974/cm-12.0`
+
+NEW_COMMIT=0
+if [[ "`git branch --contains $LAST_HASH m8cm12.0`" = "" ]]; then
+	NEW_COMMIT=1
+fi
+
+if [[ NEW_COMMIT -eq 0 ]]; then
+	exit 0;
+fi
+
+# New commit found
+git merge remotes/cm11_htc_msm8974/cm-12.0
+if [[ $? -ne 0 ]]; then
+	echo "Conflict while merging from CyanogenMod kernel https://github.com/CyanogenMod/android_kernel_htc_msm8974/commit/$LAST_HASH"
+	exit 1;
+fi
+
+make cm_m8_defconfig
+make -j1
+if [[ $? -ne 0 ]]; then
+	echo "Build failed!"
+	exit 1;
+fi
+
+git push origin
+
+cd $TOP/kitchen
+./build.sh
+./dropbox_uploader.sh upload kernel-*.zip Kernel/
+
