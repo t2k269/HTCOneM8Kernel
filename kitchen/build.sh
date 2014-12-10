98e7de8f63f03a106298d770e70ee1ed7ce975da
t2k269 <t2k269@gmail.com>
2014-12-10 16:31:04 +0800

    Add GPE 5.0.1 boot image and update the init script

--- kitchen/build.sh
+++ kitchen/build.sh
@@ -9,7 +9,7 @@ rm -f $TOP/kitchen/*.zip
 cd $TOP/source
 HASH=`git rev-parse HEAD`
 HASH=${HASH:0:7}
-SUFFIX=`grep CONFIG_LOCALVERSION= $TOP/source/arch/arm/configs/cm_m8_defconfig | awk -F '"' '{print $2}'`
+SUFFIX=`grep CONFIG_LOCALVERSION= $TOP/source/arch/arm/configs/elementalx_defconfig | awk -F '"' '{print $2}'`
 if [[ "$TOOLCHAIN" != "" ]]; then
 	SUFFIX="$SUFFIX-$TOOLCHAIN"
 fi
