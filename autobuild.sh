35cdd590f5c69f492f6e07328795ea9807558fe5
t2k269 <t2k269@gmail.com>
2015-01-13 15:43:07 +0800

    Update to CM12 nightly 20150111

--- autobuild.sh
+++ autobuild.sh
@@ -2,12 +2,12 @@
 
 cd $TOP/source
 
-git fetch cm11_htc_msm8974
+git fetch cm11_htc_msm8974 > /dev/null 2>&1
 
 LAST_HASH=`git rev-parse remotes/cm11_htc_msm8974/cm-12.0`
 
 NEW_COMMIT=0
-if [[ "`git branch --contains $LAST_HASH m8cm12.0`" == "" ]]; then
+if [[ "`git rev-parse m8cm12.0`" != "$LAST_HASH" ]]; then
 	NEW_COMMIT=1
 fi
 
@@ -15,11 +15,25 @@ if [[ NEW_COMMIT -eq 0 ]]; then
 	exit 0;
 fi
 
-# New commit found
-git merge remotes/cm11_htc_msm8974/cm-12.0
+echo New commit found
+
+git checkout m8cm12.0
+git reset --hard HEAD
+git pull
 if [[ $? -ne 0 ]]; then
+	git reset --hard HEAD
 	echo "Conflict while merging from CyanogenMod kernel https://github.com/CyanogenMod/android_kernel_htc_msm8974/commit/$LAST_HASH"
-	exit 1;
+	exit 1
+fi
+
+HEAD_HASH=`git rev-parse m8cm12.0`
+git checkout m8cm12.0_sp2w
+git reset --hard $HEAD_HASH 
+git cherry-pick 3d5f2fd3e27dacb5cb17ee26489ca9106a4f18ed 
+if [[ $? -ne 0 ]]; then
+	git cherry-pick --abort
+	echo "Unable to apply patch SP2W"
+	exit 1
 fi
 
 make cm_m8_defconfig
@@ -29,11 +43,17 @@ if [[ $? -ne 0 ]]; then
 	exit 1;
 fi
 
-git push origin
-
 cd $TOP/kitchen
 ./build.sh
 
+cd $TOP/source
+
+git push origin --force
+
+git checkout m8cm12.0
+git push origin
+
+cd $TOP/kitchen
 ZIPFILE=`ls kernel-*.zip`
 ./dropbox_uploader.sh upload $ZIPFILE Kernel/
 echo "New kernel $ZIPFILE are available in dropbox"
